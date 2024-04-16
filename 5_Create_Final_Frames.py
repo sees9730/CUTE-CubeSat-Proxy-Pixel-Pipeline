ee3ca6223afa60db0951b1924e8bf9ad4f2e209d
Sebas E <sebastian.escobar@colorado.edu>
2024-04-16 13:21:24 -0600

    Minor fixes and file management

--- 5_Create_Final_Frames.py
+++ 5_Create_Final_Frames.py
@@ -4,7 +4,7 @@ from Helper_Function.Helper import readPickleFile, outputPickleFile
 
 def infillFrame(frame, frame_fits):
     '''
-    Infill a single frame based on the final frame fits
+    Infill a single frame based on the fixed frame fits
 
     Inputs = frame (array [n x m], the frame to be infilled),
              frame_fits (array [1 x n], where n is the number of bins)
@@ -25,23 +25,23 @@ def infillFrame(frame, frame_fits):
 
     return frame
 
-# Get the final science-minus-recreated-background and the recreated-background frames from the pickle file
-frames_data = readPickleFile('Results/WASP189b_Final_Frames_Pre_Infill_v5.pbz2')
-final_frames = frames_data['Final Frames']
+# Get the fixed science-minus-recreated-background and the recreated-background frames from the pickle file
+frames_data = readPickleFile('Results/WASP189b_Fixed_Frames_Pre_Infill_v5.pbz2')
+fixed_frames = frames_data['Fixed Frames']
 background_frames = frames_data['Background Frames']
 
-# Get the final frame fits from the pickle file
-fits_data = readPickleFile('Results/WASP189b_Final_Frames_Fits_v5.pbz2')
+# Get the fixed frame fits from the pickle file
+fits_data = readPickleFile('Results/WASP189b_Fixed_Frames_Fits_v5.pbz2')
 fits = fits_data['Fits']
 
-# Infill all the images and get the infill error
-final_frames_infilled = [infillFrame(frame.copy(), fits[id]) for id, frame in enumerate(final_frames)]
+# Infill all the images
+final_frames_infilled = [infillFrame(frame.copy(), fits[id]) for id, frame in enumerate(fixed_frames)]
 
-# Remove cosmic rays from frames using lacosmic
+# Remove cosmic rays from final frames using lacosmic
 # 'Neighbor threshold' and 'cr threshold' are the modifiable parameters, the others are inherent to the CCD
 cr_removed_frames = []
 la_cosmic_mask = []
 cr_removed_frames, la_cosmic_mask = zip(*[lacosmic.lacosmic(data=frame, contrast=2, cr_threshold=6, neighbor_threshold=4, effective_gain=1.5, readnoise=4.5) for frame in final_frames_infilled])
 
 # Save the final frames
-outputPickleFile(cr_removed_frames, 'WASP189b_Final_Frames_TEST')
+outputPickleFile(cr_removed_frames, 'WASP189b_Final_Frames_v5')
